26a637491f8c6b88a55ea918c69c1edd283082f9
izenynn <me@izenynn.com>
2024-01-07 15:03:47 +0100

    feat: distro variables and packages installation

--- run_playbook.sh
+++ run_playbook.sh
@@ -2,6 +2,18 @@
 
 playbook="site.yml"
 
+# Clean up function to ensure password file is deleted
+cleanup() {
+    echo "Cleaning up..."
+    rm -f "$vault_password_file"
+}
+trap cleanup EXIT
+
+# Create a temporary file for the vault password
+vault_password_file=$(mktemp)
+echo "$ANSIBLE_VAULT_PASSWORD" > "$vault_password_file"
+echo "Info: Vault password stored in: $vault_password_file."
+
 # Check for Vault password in the environment variable and run the playbook
 if [ -z "$ANSIBLE_VAULT_PASSWORD" ]; then
     echo 'Info: Vault password environment variable (ANSIBLE_VAULT_PASSWORD) not set.'
@@ -12,6 +24,6 @@ if [ -z "$ANSIBLE_VAULT_PASSWORD" ]; then
 else
     ansible-playbook \
         "$playbook" \
-        --vault-password-file <(echo "$ANSIBLE_VAULT_PASSWORD") \
+        --vault-password-file "$vault_password_file" \
         "$@"
 fi
