ff427c25e20e1c158761e97df7959b8be7d03bff
izenynn <me@izenynn.com>
2024-01-07 15:28:39 +0100

    chore: improve run script

--- run_playbook.sh
+++ run_playbook.sh
@@ -4,15 +4,9 @@ playbook="site.yml"
 
 # Clean up function to ensure password file is deleted
 cleanup() {
-    echo "Cleaning up..."
+    echo "Cleaning up vault password file..."
     rm -f "$vault_password_file"
 }
-trap cleanup EXIT
-
-# Create a temporary file for the vault password
-vault_password_file=$(mktemp)
-echo "$ANSIBLE_VAULT_PASSWORD" > "$vault_password_file"
-echo "Info: Vault password stored in: $vault_password_file."
 
 # Check for Vault password in the environment variable and run the playbook
 if [ -z "$ANSIBLE_VAULT_PASSWORD" ]; then
@@ -22,6 +16,12 @@ if [ -z "$ANSIBLE_VAULT_PASSWORD" ]; then
         --ask-vault-pass \
         "$@"
 else
+    # Create a temporary file for the vault password
+    vault_password_file=$(mktemp)
+    echo "$ANSIBLE_VAULT_PASSWORD" > "$vault_password_file"
+    echo "Info: Vault password stored in: $vault_password_file."
+    trap cleanup EXIT
+
     ansible-playbook \
         "$playbook" \
         --vault-password-file "$vault_password_file" \
